f95fb3a91da258a97805871d8e3f08f536defd2a
koba <koba1mobile@gmail.com>
2021-01-28 13:42:41 +0900

    버튼 추가하여 클릭시 뷰 배경색 변경되도록 수정

--- app/src/main/java/com/koba/viewtypemanage/MainActivity.kt
+++ app/src/main/java/com/koba/viewtypemanage/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.koba.viewtypemanage
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.koba.viewtypemanage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        with(binding){
+            buttonBlue.setOnClickListener {
+                viewTarget.setBackgroundColor(Color.parseColor("#0011ff"))
+            }
+            buttonRed.setOnClickListener {
+                viewTarget.setBackgroundColor(Color.parseColor("#ff0000"))
+            }
+            buttonYellow.setOnClickListener {
+                viewTarget.setBackgroundColor(Color.parseColor("#fffb00"))
+            }
+        }
     }
 }
